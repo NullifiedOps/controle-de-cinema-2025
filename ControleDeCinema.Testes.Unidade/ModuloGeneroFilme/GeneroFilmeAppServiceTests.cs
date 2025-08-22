1ddf9424931cc5c2f40de76be5f7743b852e04bd
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-08-22 14:42:26 -0300

    Implementado os testes de unidade do AppService do módulo GeneroFilme

--- ControleDeCinema.Testes.Unidade/ModuloGeneroFilme/GeneroFilmeAppServiceTests.cs
+++ ControleDeCinema.Testes.Unidade/ModuloGeneroFilme/GeneroFilmeAppServiceTests.cs
@@ -31,4 +31,56 @@ public sealed class GeneroFilmeAppServiceTests : TestFixture
         Assert.IsNotNull(resultado);
         Assert.IsTrue(resultado.IsSuccess);
     }
+
+    [TestMethod]
+    public void Cadastrar_Deve_Retornar_Erro_Quando_Genero_Filme_For_Duplicada()
+    {
+        // Arrange
+        var genero = new GeneroFilme("Ação");
+        
+        var generoTeste = new GeneroFilme("Ação");
+
+        repositorioGeneroFilmeMock?
+            .Setup(r => r.SelecionarRegistros())
+            .Returns(new List<GeneroFilme>() { generoTeste });
+        
+        // Act
+        var resultado = generoFilmeAppService?.Cadastrar(genero);
+        
+        // Assert
+        repositorioGeneroFilmeMock?.Verify(r => r.Cadastrar(genero), Times.Never);
+        
+        unitOfWorkMock?.Verify(u => u.Commit(), Times.Never);
+        
+        Assert.IsNotNull(resultado);
+        Assert.IsTrue(resultado.IsFailed);
+    }
+
+    [TestMethod]
+    public void Cadastrar_Deve_Retornar_Erro_Quando_Excecao_For_Lancada()
+    {
+        // Arrange
+        var genero = new GeneroFilme("Terror");
+
+        repositorioGeneroFilmeMock?
+            .Setup(r => r.SelecionarRegistros())
+            .Returns(new List<GeneroFilme>());
+
+        unitOfWorkMock?
+            .Setup(u => u.Commit())
+            .Throws(new Exception("Erro Inesperado"));
+
+        // Act
+        var resultado = generoFilmeAppService?.Cadastrar(genero);
+
+        // Assert
+        unitOfWorkMock?.Verify(u => u.Rollback(), Times.Once);
+
+        Assert.IsNotNull(resultado);
+
+        var mensagemErro = resultado.Errors.First()?.Message;
+
+        Assert.AreEqual("Ocorreu um erro interno do servidor", mensagemErro);
+        Assert.IsTrue(resultado.IsFailed);
+    }
 }
